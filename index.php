dd75609085b145760d9f482aa2c188f16670cf85
Oday Rafeh <Udayrafehhhhh@gmail.com>
2017-10-24 15:04:55 +0200

    Oday araay

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+        <?php
+        
+       /* $nummer = array("3","4","5","6");
+        
+        
+        for($i=0;$i<8;$i++){
+            
+            echo"Oday";
+           
+            echo$i;
+            
+            echo"<br>";
+        }
+        
+        for($k=0;$k<count($nummer);$k++){
+            
+            echo "niew regel ";
+            echo $nummer[$k];
+            echo "<br>";        
+        }
+        
+        */
+        
+        $week = array("Zaterdag","zondag","mandaag","dinsdag","woensdag","donderdag","vrijdag");
+        
+        $M=0;
+        
+        for($M=1;$M<=30;$M++){
+            
+            echo "August"."  ".$M." ". ".8.2018";
+            echo "<br>";
+            $N=0;
+              
+          /* for($N =0 ;$N<1;$N++){
+                
+                echo " ". $week[0] ." ". $week[1]." ".$week[2]." ".$week[3]." ".$week[4]." ".$week[5]." ".$week[6];
+                echo "<br>";   
+                
+           }
+            */
+            
+            
+        }
+        
+        
+       ?>
